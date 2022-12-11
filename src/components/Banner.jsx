81930f506e380934f6000641db7631729aefd181
Mst Mohsina Akter <99411093+MohsinaAkter@users.noreply.github.com>
2022-12-11 13:11:49 +0600

    Add files via upload

---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Banner = () => {
+    return(
+        <div className="container">
+        <header style={{marginTop: '70px'}}>
+         <div className="headerText">
+         <div>
+         <h1>Celebrate  FIFA  world  Cup  2022</h1>
+         <p>And Enjoy BD Express online shopping choose your product</p>
+         </div>
+         </div>
+        </header>
+        </div>
+    )
+}
+
+export default Banner;
